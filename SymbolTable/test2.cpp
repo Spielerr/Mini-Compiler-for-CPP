cbe94f41362543c9d75d29abf2900b56f897b6f3
temanisparsh <sparshtemani31415@gmail.com>
2021-02-24 22:42:08 +0530

    Correct Category of variables

--- SymbolTable/test2.cpp
+++ SymbolTable/test2.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main() {
-    int a[10] = { 2, 4, c };
-    int b[];
-    int d = c(20, 40, 50);
-    b = ++a[5] +5;
+    int a = 5 + +3;
+    cout << a << endl;
     return 0;
 }
