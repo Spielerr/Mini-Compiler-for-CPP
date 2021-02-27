665a5a07c8f8fc25a07447f43a28225269c73500
sumukhaithal6 <46868064+sumukhaithal6@users.noreply.github.com>
2021-02-27 23:54:34 +0530

    added new test case

---
+++ SymbolTable/test_new_1.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+
+using namespace std;
+
+/* Note this is a comment to check the correctness of the comment removal code in Lex 
+   This comment spans multiple lines. 
+*/
+// This is a single comment line.....
+
+int main()
+{
+    // Variable declaration check
+    int a;
+    int b;
+    int c,d,e,f=5;
+    int x = 25;
+    int p = 40,q=30;
+    float m,n=3.1415f;
+    char abcdef[1000];
+    char xyz[10] = "compiler";
+    bool boo = true;
+
+    //Long name check
+    int longnameidontknowwhattowritesimplyiamwritinghopeitsenough = 200;
+
+    // Construct if check
+    //Nested if check
+    if(true)
+    {
+        if(1)
+        {
+            if(c==d)
+            {
+                if(c!=d)
+                {
+                    int a = 200;
+                }
+            }
+        }
+    }
+    if((a==b)&&(b==c)&&(d>=f)&&(p<q)||(boo))
+    {
+        cout<<"checking if multiple condiitons in if block works\n";
+    }
+
+    //If else test
+    if(m)
+    {
+        if(m==n)
+        {
+            cout<<"if else test\n";
+        }
+        else
+            c=a+b;
+    }
+    else
+        p = q*f;
+    
+    //For loop test:
+    for(int i=0;i<10;i++)
+    {
+        for(;;)
+        {
+            cout<<"for test";
+        }
+    }
+    for(;f>0;--f)
+        cout<<f<<"\t";
+    cout<<"\n";
+
+    for(int qwe=0;qwe<5;++qwe)
+    {
+        if(qwe%2!=0)
+            cout<<"qwe is odd\n";
+        else
+            cout<<"qwe is even\n";
+        if(qwe+2==5)
+            a = 20;
+    }
+}
